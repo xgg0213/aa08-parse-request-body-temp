ebb15b385181122e2cd824ea5ff4cd4664de2e52
xgg0213 <m.eringan@gmail.com>
2024-06-21 17:54:02 -0700

    phase 3 completed

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,53 @@
 function firstStep(input) {
   // Your code here 
+  const res = input.split('&');
+  console.log(res);
+  return res;
 }
 
 function secondStep(input) {
   // Your code here 
+  // input is an array of 2 strings
+  const res = input.map((el) => el.split('=') );
+  console.log(res);
+  return res;
 }
 
 function thirdStep(input) {
   // Your code here 
+  const res = input.map(([k,v]) => [k, v.replace(/\+/g, " ")]);
+  console.log(res);
+
+  return res;
 }
 
 function fourthStep(input) {
   // Your code here 
+  const res = input.map(([k, v]) => [k, decodeURIComponent(v)]);
+  console.log(res);
+
+  return res;
+
 }
 
 function fifthStep(input) {
   // Your code here 
+  return input.reduce((acc, [k,v]) => {
+    acc[k] = v;
+    return acc;
+  }, {});
 }
 
 function parseBody(str) {
   // Your code here 
+  const splitString = firstStep(str);
+  const splitKvPairs = secondStep(splitString);
+  const replacePlus = thirdStep(splitKvPairs);
+  const decodeUri = fourthStep(replacePlus);
+  const pojo = fifthStep(decodeUri);
+  console.log(pojo);
+  return pojo;
+
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -3,12 +3,31 @@ const { parseBody } = require("./parse-body");
 let server;
 
 const http = require("http");
+const { send } = require("process");
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
 server = http.createServer((req,res) => {
     console.log(req.method, req.url);
+
+    let reqBody = '';
+
+    req.on('data', (data) => {
+        reqBody += data;
+    })
+
+    
+    req.on('end', () => {
+        console.log(reqBody);
+        if (reqBody) {
+            const resBody = parseBody(reqBody);
+            req.body = resBody;
+        }
+
+        sendFormPage(req, res);
+        
+    })
 }) 
 
 const port = 8000;
