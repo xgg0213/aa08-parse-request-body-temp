476ae23a86451889087ef8a8272cd32e0038dafe
xgg0213 <m.eringan@gmail.com>
2024-06-21 17:06:30 -0700

    phase 1 completed

---
+++ server.js
@@ -0,0 +1,21 @@
+const { sendFormPage } = require("./routes");
+const { parseBody } = require("./parse-body");
+let server;
+
+const http = require("http");
+/******************************************************************************/
+/******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
+
+// Your code here
+server = http.createServer((req,res) => {
+
+}) 
+
+const port = 8000;
+
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+/******************************************************************************/
+/******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
+
+module.exports = { server };
